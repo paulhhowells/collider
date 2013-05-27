e87c322517cf23838f892f3262b9d7b6349db641
Paul H Howells <p.h.howells@gmail.com>
2013-05-27 14:38:50 +0200

    Create collider.js

---
+++ collider.js
@@ -0,0 +1,25 @@
+/*jslint bitwise: true, eqeq: false, sloppy: true, white: true, browser: true, devel: true, indent: 2 */
+/* style
+  SYMBOLIC_CONSTANTS
+  variable_names
+  $jquery_objects
+  functionNames
+  methodNames
+  ConstructorClassNames
+  css-class-names
+
+  o private object
+  r return object
+  p public object
+  
+  shorthand:
+		str - string
+		obj - object
+		arr - array
+		int - integer
+    num - number
+*/
+
+
+var phh = phh || {};
+
